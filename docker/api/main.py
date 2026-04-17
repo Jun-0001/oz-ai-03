c6f1dccb0790c5b7b9e5138e5d4829b7f0ba32e8
Jun-0001 <jtj73872711@gmail.com>
2026-04-17 15:14:02 +0900

    0417 실습파일 업로드

--- docker/api/main.py
+++ docker/api/main.py
@@ -1,19 +1,52 @@
-from fastapi import FastAPI
-from sqlalchemy import text
-from connection import SessionFactory
+import uuid
+import json
+
+from fastapi import FastAPI, Body
+from fastapi.responses import StreamingResponse
+from redis import asyncio as aredis
+
+
+# # redis://redis -> redis 프로토콜 사용:// IP 주소
+redis_client = aredis.from_url("redis://redis:6379", decode_responses=True)
 
 app = FastAPI()
 
-@app.get("/health-check")
-def health_check_handler():
-    with SessionFactory() as session:
-        stmt = text("SELECT * FROM user LIMIT 1;")
-        result = session.execute(stmt)
-        row = result.fetchone()
+@app.post("/chats")
+async def generate_chat_handler(
+    user_input: str = Body(..., embed=True),
+):
     
-    # 데이터가 없을 때를 대비한 예외 처리
-    if row is None:
-        return {"user": "None"}
+    # 2) Subscribe 채널 설정 (결과 받을 준비 중... 대기...)
+    # 비동기 작업. redis한테 나 지금 pubsub했다 알려줌
+    channel = str(uuid.uuid4())
+    pubsub = redis_client.pubsub()
+    await pubsub.subscribe(channel)
 
-    # row._mapping
-    return {"result": dict(row._mapping)}
+    # 3) Queue를 통해 Worker에 Task를 전달 (enqueue)
+    # channel: 추론 결과 여기로 보내 / user_input: 요청 내용
+    # 파이썬과 redis는 다른 형식임. redis는 문자열로만 처리
+    # 데이터를 JSON으로 만들어야함
+    task = {"channel": channel, "user_input": user_input}
+    await redis_client.lpush("queue", json.dumps(task))
+
+    # 4) 채널 메시지 읽고 토큰 반환
+    # listen() -> 대기중... -> 언제 끊김? -> 강제로 끊어야함
+    async def event_generator():
+        async for message in pubsub.listen():
+            if message["type"] != "message":
+                continue
+            
+            # DONE이라는 메시지 보내오면 강제로 멈춤
+            token = message["data"]
+            if token == "[DONE]":
+                break
+            yield token
+        
+        await pubsub.unsubscribe(channel)
+        await pubsub.close()
+
+    # 5) 추론 결과 수신
+    return StreamingResponse(
+        event_generator(),
+        media_type="text/event-stream",
+    )
--- docker/worker/main.py
+++ docker/worker/main.py
@@ -1,4 +1,9 @@
+import json
+import redis
 from llama_cpp import Llama
+
+
+redis_client = redis.from_url("redis://redis:6379", decode_responses=True)
 
 
 llm = Llama(
@@ -16,19 +21,41 @@ SYSTEM_PROMPT = (
     "Do not mix languages."
 )
 
-user_input = "Python이 뭐야?"
 
-response = llm.create_chat_completion(
-    messages=[
-        {"role": "system", "content": SYSTEM_PROMPT},
-        {"role": "user", "content": user_input},
-    ],
-    max_tokens=256,
-    temperature=0.7,
-) 
+def run():
+    while True:
+        # 1) Queue에서 task를 dequeue
+        # rpop 했을 때 문제점? -> queue 비어있으면 nill 출력
+        # brpop -> 블락 -> queue에 데이터 들어올 때까지 대기
+        _, task = redis_client.brpop("queue")
 
-print(response["choices"][0]["message"]["content"])
+        task_data: dict = json.loads(task)
+
+        # 2) (반복) 추론 -> 토큰(Streaming) -> Publish
+        response_generator = llm.create_chat_completion(
+            messages=[
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {"role": "user", "content": task_data["user_input"]},
+            ],
+            max_tokens=256,
+            temperature=0.7,
+            stream=True,
+            # 토큰 단위로 응답 -> 추론 안하고 함수(generator) 생성
+            # 즉 제너레이터 객체가 만들어짐
+        )
+
+        channel = task_data["channel"]
+        # 반복할 때마다 토큰을 chunk에 넣음
+        for chunk in response_generator:
+            token = chunk["choices"][0]["delta"].get("content")
+            if token:
+                redis_client.publish(channel, token)
+
+        # 3) 추론 종료 알림: [Done] 메시지 전송
+        redis_client.publish(channel, "[DONE]")
 
 
-
+# 이 파일이 직접 실행한 경우에만, run() 호출
+if __name__ == "__main__":
+    run()
 
